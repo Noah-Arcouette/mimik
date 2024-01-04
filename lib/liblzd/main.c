6ae9da29f53bdb32e304f31492eccc20cc21bf92
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2024-01-04 13:08:54 -0700

    setup lzd testing environment

---
+++ lib/liblzd/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int
+main (void)
+{
+    return 0;
+}
+
+/*
+
+ - Buffered Reading/Writing
+ - Dictionary
+ - Window
+ - History
+ - Matching
+ - Move-To-Front
+ - Encoding
+
+*/
