1c8fb9ab17a264d43e80feca385015a4f97dc76e
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2024-01-12 13:27:42 -0700

    feature/fio: add fflush

---
+++ lib/libc/stdio/fflush.c
@@ -0,0 +1,72 @@
+#include <unistd.h>
+#include "stdio.h"
+#include <errno.h>
+
+int
+fflush (FILE *stream)
+{
+    if (!stream)
+    {
+        for (size_t i = 0; i<FOPEN_MAX; i++)
+        {
+            if (__stdio_files[i].flags & _FILE_FLAG_IN_USE)
+            {
+                if (fflush(&__stdio_files[i]))
+                {
+                    return EOF;
+                }
+            }
+        }
+        return 0;
+    }
+
+    if (!(stream->flags & _FILE_FLAG_IN_USE))
+    {
+        errno = EINVAL;
+        return EOF;
+    }
+
+    // nothing to do
+    if (!(stream->flags & (_FILE_FLAG_BUFFERED | _FILE_FLAG_LINE_BUFFERED)) || !stream->buffuse)
+    {
+        return 0;
+    }
+
+    // flush read buffer
+    if (stream->flags & _FILE_FLAG_LAST_READ)
+    {
+        off_t seek = lseek(stream->fildes, 0, SEEK_END);
+        if (seek >= 0)
+        {
+            stream->seek = seek;
+        }
+
+        stream->buffuse = 0;
+        stream->flags  |= _FILE_FLAG_EOF;
+        stream->flags  &= ~_FILE_FLAG_LAST_READ;
+        return 0;
+    }
+
+    // flush write buffer
+    if (stream->flags & _FILE_FLAG_LAST_WRITE)
+    {
+        if (!(stream->flags & _FILE_FLAG_FILDES))
+        {
+            return 0;
+        }
+
+        if (pwrite(stream->fildes, stream->buff, stream->buffuse, stream->seek) != (ssize_t)stream->buffuse)
+        {
+            stream->flags |= _FILE_FLAG_ERROR;
+            return EOF;
+        }
+
+        stream->seek   += stream->buffuse;
+        stream->buffuse = 0;
+        stream->flags  &= ~_FILE_FLAG_LAST_WRITE;
+        return 0;
+    }
+
+    // nothing to flush
+    return 0;
+}
---
+++ share/en_CA.ascii/man3/fflush.md
@@ -0,0 +1,74 @@
+# Prologue
+
+This manual is part of the Mimik System Tree, other implementations may differ. Some compatibility with existing standards may be met but to do not expect any.
+
+
+# Name
+
+fflush - Flush the contents of a stream buffer, part of libc
+
+
+# Synopsis
+
+```C
+#include <stdio.h>
+
+int fflush (FILE *restrict stream);
+```
+
+
+# Description
+
+*fflush* shall flush the contents of *stream*. If the buffer is in writing mode the contents shall be written to the associated file descriptor. If the buffer is in reading mode the contents shall be discarded and the seek value set to the end of the file descriptor. If *stream* is NULL then *fflush* shall flush the buffers for all streams in use.
+
+
+# Environment Variables
+
+Environment variables shall not be consider or modified by this function.
+
+
+# Errors
+
+*fflush* shall set errno and return negative one on error.
+
+## EINVAL
+
+*stream* is invalid.
+
+## ENOMEM
+
+No enough memory for writing.
+
+## EFBIG
+
+Writing will exceed the file limit.
+
+## ENOSPC
+
+No space left of underlying device.
+
+## EBADF
+
+Underlying file descriptor is invalid.
+
+
+# Extended Description
+
+There is no extended description.
+
+
+# Future Directions
+
+There are no future directions.
+
+
+# Compliance
+
+This function shall comply with:
+
+The Open Group Base Specifications Issue 7, 2018 edition
+IEEE Std 1003.1-2017 (Revision of IEEE Std 1003.1-2008)
+Copyright (c) 2001-2018 IEEE and The Open Group
+
+
+# See Also
