15cc5ee6fc2be7025f3220c67d4a3e76e627f31e
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2024-01-12 14:09:10 -0700

    feature/fio: add fwrite.c

---
+++ lib/libc/stdio/fwrite.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+#include <stddef.h>
+
+size_t 
+fwrite (const void *restrict buf, size_t itemsz, size_t items, FILE *restrict stream)
+{
+    for (size_t item = 0; item<items; item++)
+    {
+        for (size_t i = 0; i<itemsz; i++)
+        {
+            if (fputc(*(unsigned char *)buf, stream))
+            {
+                return item;
+            }
+            buf++;
+        }
+    }
+    return items;
+}
