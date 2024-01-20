1e36fa6caac2684620995087389a00ea2aa0d46a
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2024-01-19 21:23:22 -0700

    feature/fio: update stack smash protector

--- lib/libc/arch/linux-x64/smash.c
+++ lib/libc/arch/linux-x64/smash.c
@@ -1,10 +1,14 @@
 #include <stdint.h>
-#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
 
 uintptr_t __stack_chk_guard = 0xa7ee33098b32891a;
 
 void
 __stack_chk_fail (void)
 {
-    abort();
+    fputs("Stack smash detected, force exiting...\n", stderr);
+
+    _exit(EFAULT);
 }
