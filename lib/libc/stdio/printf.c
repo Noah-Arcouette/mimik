b4729482e7aa41a50be91c5c571f8a44a3e94150
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2024-01-30 15:33:15 -0700

    add printf and fprintf

---
+++ lib/libc/stdio/printf.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int
+printf (const char *fmt, ...)
+{
+    va_list vargs;
+    va_start(vargs, fmt);
+
+    int ret = vfprintf(stdout, fmt, vargs);
+
+    va_end(vargs);
+
+    return ret;
+}
+
+int
+fprintf (FILE *restrict file, const char *restrict fmt, ...)
+{
+    va_list vargs;
+    va_start(vargs, fmt);
+
+    int ret = vfprintf(file, fmt, vargs);
+
+    va_end(vargs);
+
+    return ret;
+}
