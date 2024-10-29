ea003797bc82fe09cb0100ccd2fc46286a44fe31
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2024-10-29 17:03:27 -0600

    usr.bin/cc: restart c compiler

--- usr.bin/cc/cc.c
+++ usr.bin/cc/cc.c
@@ -1,27 +1,8 @@
-//#include "defs.h"
 #include <stdio.h>
 
-extern int  yyparse       (void);
-extern void yylex_destroy (void);
-
-      int   errors = 0;
-const char *self   = "usr.bin/cc";
-
-const char *filename = "<stdin>";
-
 int
-main (int argc, char *const *argv)
+main (void)
 {
-	if (argc > 0)
-	{
-		self = argv[0]; // get program name
-	}
-
-	// parse file
-	yyparse();
-
-	yylex_destroy(); // free the lexer
-
-	return errors;
+	printf("Hello, world!\n");
+	return 0;
 }
-
