4e246fd8d0322f2d41bf94ec1802306180319172
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2024-11-12 16:01:14 -0700

    usr.bin/cc: add external

---
+++ usr.bin/cc/parser/extern.c
@@ -0,0 +1,39 @@
+#include "../defs.h"
+
+int
+extern_ (void)
+{
+	if (token != EXTERN)
+	{
+		return 1;
+	}
+	token = (enum token)yylex(); // accept
+
+	struct type t;
+	if (type(&t))
+	{
+		fprintf(stderr, "%s:%zu: Expected a type after keyword extern.\n", filename, lineno);
+		errors++;
+		recover();
+		return 1;
+	}
+
+	if (token != SYMBOL)
+	{
+		fprintf(stderr, "%s:%zu: Expected a symbol name after type.\n", filename, lineno);
+		errors++;
+		recover();
+		return 1;
+	}
+	token = (enum token)yylex(); // accept
+
+	if (token != SEMICOLON)
+	{
+		fprintf(stderr, "%s:%zu: Missing semicolon.\n", filename, lineno);
+		errors++;
+		return 1;
+	}
+	token = (enum token)yylex();
+
+	return 0;
+}
