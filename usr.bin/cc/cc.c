7122ffcbcb17afe6e9e075be944c38cd01ee5357
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2024-11-12 14:45:51 -0700

    usr.bin/cc: murder all my hard work

--- usr.bin/cc/cc.c
+++ usr.bin/cc/cc.c
@@ -1,29 +1,7 @@
-#include "defs.h"
-#include <stdlib.h>
 #include <stdio.h>
 
-	  FILE *fout;
-const char *filename;
-const char *self = "cc";
-
 int
-main (int argc, const char **argv)
+main (void)
 {
-	if (argc > 0)
-	{
-		self = argv[0];
-	}
-
-	atexit((void(*)(void))yylex_destroy);   // free lex at program exit
-	atexit(               freeAllContexts); // free contexts
-	atexit(               freeInfo);        // free information stack
-
-	fout     = stdout; // output for standard out
-	filename = "<stdin>";
-
-	// parse input
-	yyparse();
-
-	return errors;
+	return 0;
 }
-
