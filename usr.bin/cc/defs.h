a5e8121390ad02a7c4020adaa4cfd7089c89274d
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2024-11-12 17:19:10 -0700

    usr.bin/cc: add pointers and more complex types

--- usr.bin/cc/defs.h
+++ usr.bin/cc/defs.h
@@ -5,11 +5,22 @@ enum token {
 	// = Key Words =
 	EXTERN = 256,
 	// = Type =
+	VOID,
+	CHAR,
+	SHORT,
 	INT,
+	// = Qualifier =
+	UNSIGNED,
+	SIGNED,
+	LONG,
+	VOLATILE,
+	CONST,
+	RESTRICT,
 	// = Symbol =
 	SYMBOL,
 	// = Characters =
-	SEMICOLON
+	SEMICOLON,
+	STAR
 };
 
 // lexer.l
@@ -23,14 +34,28 @@ extern char  *yytext;
 extern const char *filename;
 extern enum token  token;
 extern size_t      errors;
+extern const char *self;
 
-// <type>
+// type.c
 struct type
 {
 	enum {
-		TYPE_INT
+		TYPE_VOID,
+		TYPE_CHAR,
+		TYPE_SHORT,
+		TYPE_INT,
+		TYPE_POINTER,
 	} base;
+	int isVolatile : 1;
+	int isConst    : 1;
+	int isRestrict : 1;
+	int longness   : 2;
+	int isUnsigned : 1; // unsigned?
+	struct type *down; // if pointer, this is the sub-type
 };
+
+extern void printType (struct type);
+extern void freeType  (struct type);
 
 // parser/extern.c
 extern int extern_ (void);
