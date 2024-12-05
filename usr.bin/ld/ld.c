e9354dd02c095155ef1e8e88e857ac013f2afe15
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2024-12-05 16:56:39 -0700

    usr.bin/ld: basic setup

---
+++ usr.bin/ld/ld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int
+main (void)
+{
+	// parse arguments (linker script, input files, output file)
+	// parse script (input files, output file, entry, abi/run-time information, memory map, sections, symbols, gaps)
+	// fill as many gaps as possible
+	// emit output file
+	return 0;
+}
