ec93f0cd0a1b67d3df886e8bf564a67ba59c1086
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2024-12-06 20:06:24 -0700

    usr.bin/readmio: add section enumeration

--- usr.bin/readmio/printmio.c
+++ usr.bin/readmio/printmio.c
@@ -9,7 +9,9 @@ printMiO (FILE *f)
 {
 	// read headers
 	size_t headerIdx = 0;
-	struct MiO_Header header;
+	size_t sectionIdx;
+	struct MiO_Header  header;
+	struct MiO_Section section;
 	do
 	{
 		printf("\nHeader[%zu]:\n", headerIdx); // say what header we're in
@@ -17,26 +19,41 @@ printMiO (FILE *f)
 		// read the header
 		if (fread(&header, sizeof(struct MiO_Header), 1, f) != 1)
 		{
-			printError(errno, "Failed to read header.\n", headerIdx);
-			break; // leave
+			printError(errno, "Failed to read header.\n");
+			return; // leave
 		}
 
 		// validate the header
 		if (memcmp(header.magic, MIO_MAGIC, sizeof(header.magic)))
 		{
-			printError(0, "Corrupt magic number, got `%.*s'.\n", headerIdx, sizeof(header.magic), header.magic);
-			break; // leave
+			printError(0, "Corrupt magic number, got `%.*s'.\n", sizeof(header.magic), header.magic);
+			return; // leave
 		}
 		if (header.version != MIO_CURRENT_VERSION)
 		{
-			printError(0, "Unsupported MiO version, got %d.\n", headerIdx, header.version);
+			printError(0, "Unsupported MiO version, got %d.\n", header.version);
+			return; // leave
 		}
 
 		// print header
 		printHeader(header);
 
 		// print sections
-		//   until last section
+		sectionIdx = 0;
+		do {
+			printf("\nSection[%zu]:\n", sectionIdx);
+
+			// read in the section
+			if (fread(&section, sizeof(struct MiO_Section), 1, f) != 1)
+			{
+				// failed
+				printError(errno, "Failed to read section header.\n");
+				return;
+			}
+
+			sectionIdx++; // next section
+		} while(!(section.flags & MIO_SECTION_FLAG_LAST)); // until last section
+		// read data
 
 		headerIdx++; // next header
 	} while (!(header.flags & MIO_HEADER_FLAG_LAST)); // until last header
