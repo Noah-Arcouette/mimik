2085d7d2ab595186d5c6aff2f218bee06f6d10d2
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2026-05-26 21:03:43 -0600

    usr.bin/as: add assembler layout

--- usr.bin/as/main.c
+++ usr.bin/as/main.c
@@ -1,5 +1,20 @@
 int
 main (void)
 {
+	// parse arguments
+	// lexer
+	// parser:
+	//  - sections
+	//  - architecture
+	//  - symbols
+	//  - gaps
+	//  - emit data
+	//  - reserve data
+	//  - x86-16
+	// create architecture section
+	// create symbol section
+	// create gap section
+	// emit the mio file
+
 	return 1;
 }
