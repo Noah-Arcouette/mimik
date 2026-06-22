5a90ceb345ea44b4ebb902b10be2dc228740be07
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2026-06-22 11:00:12 -0600

    usr.bin/as: add `nop` instruction

--- usr.bin/as/x86_16/parse.c
+++ usr.bin/as/x86_16/parse.c
@@ -8,6 +8,7 @@ parse_x86_16 (void)
 
 	if (parse_x86_16_singlet("cli", 0b11111010)) return 1;
 	if (parse_x86_16_singlet("hlt", 0b11110100)) return 1;
+	if (parse_x86_16_singlet("nop", 0b10010000)) return 1;
 
 	return 0;
 }
