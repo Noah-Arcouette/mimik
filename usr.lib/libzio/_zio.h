e9cfa7e43ae6bdfff9c6f0599e0b33b08ebf3ed4
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2026-07-16 13:57:07 -0600

    usr.lib/libzio: add default evaluation

--- usr.lib/libzio/_zio.h
+++ usr.lib/libzio/_zio.h
@@ -4,4 +4,15 @@
 
 // Provide actual definitions and internal definitions
 
+// Compression default
+#if defined(DEFAULT_LZW)
+#	define _ZIO_DEFAULT_COMPRESS ZIO_FORMAT_LZW
+#elif defined(DEFAULT_DEFLATE)
+#	define _ZIO_DEFAULT_COMPRESS ZIO_FORMAT_DEFLATE
+#elif defined(DEFAULT_GZIP)
+#	define _ZIO_DEFAULT_COMPRESS ZIO_FORMAT_GZIP
+#else
+#	define _ZIO_DEFAULT_COMPRESS ZIO_FORMAT_GZIP // why not
+#endif
+
 #endif
