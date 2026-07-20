9270cad86c2869767c5bedfeadc612569517b915
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2026-07-20 15:47:08 -0500

    usr.lib/libzio: add _zio_write_fd

---
+++ usr.lib/libzio/fd/_zio_write_fd.c
@@ -0,0 +1,37 @@
+#include "../_zio.h"
+#include <unistd.h>
+#include <string.h>
+
+size_t
+_zio_write_fd (zFILE *restrict fp, const void *restrict buf, size_t amt)
+{
+	char   *inbuf = fp->fd.buf;
+	size_t *bufsz = &fp->fd.bufsz;
+	const ssize_t bufcp = fp->fd.bufcp;
+
+	size_t amountWritten = 0;
+
+	while (1)
+	{
+		size_t amountLeft = bufcp-*bufsz; // amount left to fill in buffer
+		if (amt > amountLeft)
+		{
+			// save it
+			memcpy(inbuf+*bufsz, buf, amountLeft);
+			*bufsz = 0;
+
+			// dump it all
+			if (write(fp->fd.fd, inbuf, bufcp) < bufcp)
+			{
+				return amountWritten;
+			}
+			amountWritten += bufcp;
+			buf += amountLeft;
+			continue;
+		}
+		// else
+		memcpy(inbuf+*bufsz, buf, amt);
+		*bufsz += amt;
+		return amountWritten;
+	}
+}
