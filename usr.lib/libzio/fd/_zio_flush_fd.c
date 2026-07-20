b1f4b5085a2c9298f0594091aaabb6bd1c81e5a5
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2026-07-20 15:39:02 -0500

    usr.lib/libzio; add _zio_flush_fd

---
+++ usr.lib/libzio/fd/_zio_flush_fd.c
@@ -0,0 +1,17 @@
+#include "../_zio.h"
+#include <unistd.h>
+
+int
+_zio_flush_fd (zFILE *fp)
+{
+	ssize_t bufsz = fp->fd.bufsz;
+	fp->fd.bufof = 0;
+	fp->fd.bufsz = 0;
+
+	if (fp->rw == _ZFILE_RW_WRITING)
+	{
+		// write the end to the file
+		if (write(fp->fd.fd, fp->fd.buf, bufsz) < bufsz) return 1;
+	}
+	return 0;
+}
