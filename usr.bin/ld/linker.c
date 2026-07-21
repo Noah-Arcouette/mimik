d4b82cb55ef2f2a6781ac03cee820e89bf65f243
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2026-07-21 13:31:11 -0500

    usr.bin/ld: remove lexer testing

--- usr.bin/ld/linker.c
+++ usr.bin/ld/linker.c
@@ -19,70 +19,6 @@ linker (void)
 	}
 	lfilename = scriptFile;
 
-	// print stuff out
-	do
-	{
-		lex();
-
-		switch (ltoken.type)
-		{
-		// symbols
-		case LTYPE_OPEN_CURLY:
-		case LTYPE_CLOSE_CURLY:
-		case LTYPE_EQUAL:
-		case LTYPE_SEMICOLON:
-		case LTYPE_COLON:
-		case LTYPE_OPEN_PAREN:
-		case LTYPE_CLOSE_PAREN:
-		case LTYPE_PLUS:
-		case LTYPE_SUBTRACT:
-		case LTYPE_DIVIDE:
-		case LTYPE_MULTIPLY:
-		case LTYPE_PERIOD:
-			printf("%c ", ltoken.type);
-			break;
-		// others
-		case LTYPE_EOF:
-			printf("eof\n");
-			break;
-		case LTYPE_UNKNOWN:
-			printf("unk<%s> ", ltoken.buf);
-			break;
-		// macros
-		case LTYPE_ENTRY:
-			printf("entry ");
-			break;
-		case LTYPE_ARCH:
-			printf("arch ");
-			break;
-		case LTYPE_UARCH:
-			printf("uarch ");
-			break;
-		case LTYPE_ARCHFLAGS:
-			printf("archflags ");
-			break;
-		case LTYPE_SYS:
-			printf("sys ");
-			break;
-		case LTYPE_USYS:
-			printf("usys ");
-			break;
-		case LTYPE_SYSFLAGS:
-			printf("sysflags ");
-			break;
-		case LTYPE_SECTIONS:
-			printf("sections ");
-			break;
-		// values
-		case LTYPE_SYMBOL:
-			printf("sym<%s> ", ltoken.buf);
-			break;
-		case LTYPE_INTEGER:
-			printf("int<%s> ", ltoken.buf);
-			break;
-		}
-	} while (ltoken.type != LTYPE_EOF);
-
 	// ENTRY(symbol)
 	// ARCH(arch)
 	// UARCH(uarch)
@@ -95,7 +31,7 @@ linker (void)
 	//   symbol = expr;
 	//   section [expr] [(noload)] : [expr]
 	//   {
-	//      file(section...)
+	//      file(section...) // glob matching
 	//      ...
 	//   }
 	//   ...
