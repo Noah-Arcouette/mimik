3a2577d132d0dc5367ea51b8fa2005f1ddb9fb84
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2026-07-23 15:21:38 -0500

    usr.bin/ld: add findSymbol

---
+++ usr.bin/ld/findSymbol.c
@@ -0,0 +1,31 @@
+#include <libintl.h>
+#include <endian.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+struct MiO_Symbol *
+findSymbol (const char *name)
+{
+	// for each input
+	for (long i = 0; i<inputs; i++)
+	{
+		struct inputFile *file = &input[i];
+
+		// no symbols
+		if (!file->symbol) continue;
+
+		// for each symbol
+		for (long j = 0; j<file->symbols; j++)
+		{
+			struct MiO_Symbol *symbol = &input->symbol[j];
+
+			if (!strncmp(symbol->name, name, sizeof(symbol->name)))
+			{
+				return symbol;
+			}
+		}
+	}
+	return NULL;
+}
