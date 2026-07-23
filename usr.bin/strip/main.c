567661ad56ff9a4a703fc4b247da92c9aeedac5c
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2026-07-23 17:47:09 -0500

    usr.bin/strip: add outline

--- usr.bin/strip/main.c
+++ usr.bin/strip/main.c
@@ -1,6 +1,19 @@
 int
 main (void)
 {
+	// dual buffer system
+
+	// load file
+
+	// execute LTO
+	// swap buffers
+	// execute next LTO
+	// swap buffers
+	// ...
+	// no LTOs modified anything, break
+
+	// store file
+
 	// do nothing
 	return 0;
 }
