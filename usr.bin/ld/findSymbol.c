674e00dac4b67c7f1a853f9bd6fd741d89b10e84
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2026-07-29 16:58:41 -0500

    usr.bin/ld: find symbols from output

--- usr.bin/ld/findSymbol.c
+++ usr.bin/ld/findSymbol.c
@@ -8,6 +8,17 @@
 struct MiO_Symbol *
 findSymbol (const char *name)
 {
+	// check the output symbols fist
+	for (size_t i = 0; i<outputSymbolsz; i++)
+	{
+		struct MiO_Symbol *sym = &outputSymbol[i];
+		// symbol found
+		if (strncmp((char *)sym->name, name, sizeof(sym->name)))
+		{
+			return sym;
+		}
+	}
+
 	// for each input
 	for (long i = 0; i<inputs; i++)
 	{
