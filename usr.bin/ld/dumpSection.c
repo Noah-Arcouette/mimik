bad21568afda19edbd48326b1a19244edb42c3f6
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2026-07-30 16:04:20 -0500

    usr.bin/ld: move symbols with the sections

--- usr.bin/ld/dumpSection.c
+++ usr.bin/ld/dumpSection.c
@@ -23,19 +23,106 @@ did you forget `NOLOAD'?\n"),
 		return;
 	}
 
+	int maptls = map->flags & MIO_MAP_FLAG_THREAD_LOCAL;
+	if (maptls > 0) maptls = 1;
+	if (map->size == 0)
+	{
+		maptls = -1; // not set
+	}
+
 	// emit/reserve section (mark it)
 	inpsection->name[0] = '\0';
+	long sectionSize = le64toh(inpsection->size);
+
+	long newSectionStart;
+
 	if (inpvirtual)
 	{
-		reserve(le64toh(inpsection->size));
+		newSectionStart = virtualsz;
+		reserve(sectionSize);
 	}
 	else
 	{
+		newSectionStart = outputsz;
 		char *dat = inpfile->data+offset+sizeof(struct MiO);
-		emit(dat, le64toh(inpsection->size));
+		emit(dat, sectionSize);
 	}
 
+	long sectionStart = offset+sizeof(struct MiO);
+	long sectionEnd   = sectionStart+sectionSize;
+
 	// copy symbols (mark them)
+	for (long i = 0; i<inpfile->symbols; i++)
+	{
+		struct MiO_Symbol *inpsym = &inpfile->symbol[i];
+
+		int symFlags = le16toh(inpsym->flags);
+
+		// literals don't count
+		if (symFlags & MIO_SYMBOL_FLAG_LITERAL) continue;
+
+		// can't mix data and virtual
+		if ((symFlags & MIO_SYMBOL_FLAG_VIRTUAL) != inpvirtual) continue;
+
+		long symStart = le64toh(inpsym->value);
+
+		// symbol is not in the section
+		if (symStart < sectionStart || symStart > sectionEnd) continue;
+
+		// relocate the symbol
+		long symReloc = sectionStart-symStart+newSectionStart;
+
+		// move the symbol over
+		newSymbol(symReloc, le64toh(inpsym->size), symFlags,
+			(char *)inpsym->name);
+
+		// mark the symbol
+		inpsym->name[0] = '\0';
+
+		// set flags
+		if (symFlags & MIO_SYMBOL_FLAG_EXECUTABLE)
+		{
+			map->flags |= MIO_MAP_FLAG_EXECUTABLE;
+		}
+		if (symFlags & MIO_SYMBOL_FLAG_READABLE)
+		{
+			map->flags |= MIO_MAP_FLAG_READABLE;
+		}
+		if (symFlags & MIO_SYMBOL_FLAG_WRITABLE)
+		{
+			map->flags |= MIO_MAP_FLAG_WRITABLE;
+		}
+
+		switch (maptls)
+		{
+		case 1:
+			if (!(symFlags & MIO_SYMBOL_FLAG_THREAD_LOCAL))
+			{
+				prettyprint(gettext(
+					"Refusing to move non-TLS `%s:%.*s' into TLS map\n"),
+					inpfile->path, 256, inpsection->name);
+				errors++;
+			}
+			break;
+		case 0:
+			if (symFlags & MIO_SYMBOL_FLAG_THREAD_LOCAL)
+			{
+				prettyprint(gettext(
+					"Refusing to move TLS `%s:%.*s' into non-TLS map\n"),
+					inpfile->path, 256, inpsection->name);
+				errors++;
+			}
+			break;
+		case -1:
+			maptls = symFlags & MIO_SYMBOL_FLAG_THREAD_LOCAL;
+			if (maptls)
+			{
+				map->flags |= MIO_MAP_FLAG_THREAD_LOCAL;
+			}
+			break;
+		}
+	}
+
 	// copy gaps (mark them)
 }
 
