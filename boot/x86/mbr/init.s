0601c79478f00f694e2fd2476e026c681991d484
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2026-08-03 18:05:58 -0500

    boot/x86/mbr: add outline for boot loader

--- boot/x86/mbr/init.s
+++ boot/x86/mbr/init.s
@@ -2,6 +2,19 @@
 .section .init x
 
 _init:
+	; setup segments
+	xor %ax %ax
+	; mov %es %ax
+	; mov %ds %ax
+	; mov %ss %ax
+	; mov %sp BSS_END
+	; mov %bp %sp
+	; ljmp 0 _init.next
+_init.next:
+	; move the boot code
+	; find bootable partition
+	; load the partition and jump to it
+
 	jmp halt
 
 .global _init
