00810443cb29aa417d481ebb23ab6b564d67d904
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2026-08-04 13:04:17 -0500

    boot/x86/mbr: setup the mbr so it can be relocated

--- boot/x86/mbr/init.s
+++ boot/x86/mbr/init.s
@@ -2,23 +2,25 @@
 .section .init x
 
 _init:
-	; setup segments
+	; setup segments, other than cs as our addresses are wrong
 	xor %ax %ax
 	mov %es %ax
 	mov %ds %ax
 	mov %ss %ax
 	mov %sp BSS_END
 	mov %bp %sp
-	ljmp _init.next 0
-_init.next:
-	; move the boot code
+
+	mov %si 0x7c00
+	mov %di 0x600
+	mov %cx 0x100
+	; repz movs .word
+
+	; addresses are good now
+	ljmp _start 0
+_start:
 	; find bootable partition
 	; load the partition and jump to it
 
 	jmp halt
 
 .global _init
-
-.section .data rw
-_data:
-	jmp _init
