af16ba35d95ccfc81de657c68d54cd7b61506735
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2026-08-06 14:06:34 -0500

    usr.sbin/fdisk/mbr: MBR ids are 32bit not 16

--- usr.sbin/fdisk/mbr/listParts.c
+++ usr.sbin/fdisk/mbr/listParts.c
@@ -11,7 +11,7 @@ mbr_listParts (void)
 		fprintf(stderr, gettext("%s: No MBR\n"), self);
 		return;
 	}
-	fprintf(stderr, gettext("%s:\tMBR\tID %04x\n"), self, le16toh(mbr.id));
+	fprintf(stderr, gettext("%s:\tMBR\tID %08x\n"), self, le32toh(mbr.id));
 	// else
 
 	for (int i = 0; i<4; i++)
