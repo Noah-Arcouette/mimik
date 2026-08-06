ff0d7551471ee8ae0bf8f1f71826044e8366c2a8
Noah Arcouette @G5-5000 <kcu9626@gmail.com>
2026-08-05 21:09:05 -0500

    usr.sbin/fdisk/mbr: add boot flags and change formatting

--- usr.sbin/fdisk/mbr/listParts.c
+++ usr.sbin/fdisk/mbr/listParts.c
@@ -8,12 +8,11 @@ mbr_listParts (void)
 {
 	if (!writeMBR)
 	{
-		fprintf(stderr, gettext("%s: No MBR\n\n"), self);
+		fprintf(stderr, gettext("%s: No MBR\n"), self);
 		return;
 	}
 	// else
 
-	fprintf(stderr, gettext("%s: MBR:\n"), self);
 	for (int i = 0; i<4; i++)
 	{
 		struct mbr_part *p = &mbr.part[i];
@@ -22,40 +21,45 @@ mbr_listParts (void)
 		if (!p->type) continue;
 
 		// print the partition
-		fprintf(stderr, gettext("%s: Part #%d : Start "), self, i);
+		fprintf(stderr, gettext("%s:\tMBR\tPart #%d\tStart "), self, i);
 		printSize(le32toh(p->firstLBA)*bytesPerSector);
-		fprintf(stderr, gettext(" (%d/%d/%d) : End "),
+		fprintf(stderr, gettext(" (%d/%d/%d)\tEnd "),
 			p->firstCHS[2]|((p->firstCHS[1]&0xc0)<<2),
 			p->firstCHS[0],
 			p->firstCHS[1]&0x3f);
 		printSize((le32toh(p->firstLBA)+le32toh(p->sectors))*bytesPerSector);
-		fprintf(stderr, gettext(" (%d/%d/%d) : Size "),
+		fprintf(stderr, gettext(" (%d/%d/%d)\tSize "),
 			p->lastCHS[2]|((p->lastCHS[1]&0xc0)<<2),
 			p->lastCHS[0],
 			p->lastCHS[1]&0x3f);
 		printSize(le32toh(p->sectors)*bytesPerSector);
-		fprintf(stderr, gettext(" : Type %02x ("), p->type);
+		fprintf(stderr, gettext("\tType %02x ("), p->type);
 		switch (p->type)
 		{
 		case MBR_PART_TYPE_VBR:
-			fprintf(stderr, gettext("VBR)\n"));
+			fprintf(stderr, gettext("VBR"));
 			break;
 		case MBR_PART_TYPE_ROOT:
-			fprintf(stderr, gettext("Root FS)\n"));
+			fprintf(stderr, gettext("Root FS"));
 			break;
 		case MBR_PART_TYPE_FS:
-			fprintf(stderr, gettext("FS)\n"));
+			fprintf(stderr, gettext("FS"));
 			break;
 		case MBR_PART_TYPE_GPT_PROT:
-			fprintf(stderr, gettext("GPT Protected)\n"));
+			fprintf(stderr, gettext("GPT Protected"));
 			break;
 		case MBR_PART_TYPE_ESP:
-			fprintf(stderr, gettext("uEFI Boot)\n"));
+			fprintf(stderr, gettext("uEFI Boot"));
 			break;
 		default:
-			fprintf(stderr, gettext("Unknown)\n"));
+			fprintf(stderr, gettext("Unknown"));
 			break;
 		}
+		fprintf(stderr, gettext(")\tFlags %02x ( "), p->flags);
+		if (p->flags & MBR_PART_FLAG_BOOTABLE)
+		{
+			fprintf(stderr, " Bootable");
+		}
+		fprintf(stderr, gettext(" )\n"));
 	}
-	fprintf(stderr, "\n");
 }
